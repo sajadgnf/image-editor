fd6df414c1bc5a8789dd3e386f26c46d5303683d
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2022-02-25 17:55:16 +0330

    add drag and drop effect

--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,11 +1,12 @@
 import React, { useContext, useRef, useState } from 'react';
 import domtoimage from "dom-to-image"
+import { useDropzone } from "react-dropzone"
 
 // styles
 import styles from "./ImageDisplay.module.scss"
 
 // icons 
-import { BsImageFill, BsFillTrashFill } from "react-icons/bs"
+import { BsImageFill, BsFillTrashFill, BsPlusLg } from "react-icons/bs"
 import { ImDownload } from "react-icons/im"
 
 // context
@@ -15,9 +16,11 @@ const ImageDisplay = ({ dark }) => {
 
     const { state } = useContext(editorContext)
     const [uploadedImage, setUploadedImage] = useState(null)
+    const [file, setFile] = useState([])
     const btnRef = useRef()
     const imageRef = useRef()
 
+    // image uploader
     const imageHandler = event => {
         if (event.target.files && event.target.files[0]) {
             const image = event.target.files[0]
@@ -25,8 +28,24 @@ const ImageDisplay = ({ dark }) => {
         }
     }
 
+    //drag and drop image
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/*",
+        onDrop: (acceptedFiles) => {
+            setFile(
+                acceptedFiles.map((file) =>
+                    Object.assign(file, {
+                        preview: URL.createObjectURL(file),
+                    })
+                )
+            )
+        },
+    })
+
+    //remove image handler
     const removeHandler = () => {
         setUploadedImage(null)
+        setFile([])
         btnRef.current.value = ""
     }
 
@@ -71,23 +90,49 @@ const ImageDisplay = ({ dark }) => {
                 link.download = 'my-image.png';
                 link.href = dataUrl;
                 link.click();
-              });
+            });
     }
-
+    console.log(window.innerWidth);
     return (
         <div className={`${styles.imageDisplay} ${dark ? styles.darkImageDisplay : ''}`}>
-            <div className={styles.imageContainer}>
+
+            <div className={styles.imageContainer} {...getRootProps()}>
                 {
-                    uploadedImage &&
-                    <img ref={imageRef} className={styles.image} src={uploadedImage} alt="image" />
+                    window.innerWidth > 720 && !file.length && !uploadedImage &&
+                    <div className={styles.dropContainer}>
+                        <input {...getInputProps()} />
+                        <p>Drop Image Here</p>
+                        <BsPlusLg />
+                    </div>
+                }
+                {
+                    file.length || uploadedImage ?
+                        <img
+                            ref={imageRef}
+                            className={styles.image}
+                            src={
+                                uploadedImage && uploadedImage ||
+                                file.length && file[0].preview
+                            }
+                            alt="image"
+                        /> :
+                        null
                 }
             </div>
 
             <div className={`${styles.icons} ${dark ? styles.darkIcons : ''}`}>
                 <label className={styles.uploadIcon} htmlFor="image_input">
                     <BsImageFill />
-                    <input ref={btnRef} className={styles.imageInput} type="file" id='image_input' accept='image/png, iage/jpg' onChange={event => imageHandler(event)} />
+                    <input
+                        ref={btnRef}
+                        className={styles.imageInput}
+                        type="file"
+                        id='image_input'
+                        accept='image/png, iage/jpg'
+                        onChange={event => imageHandler(event)}
+                    />
                 </label>
+
                 <ImDownload onClick={downloadHandler} />
                 <BsFillTrashFill onClick={removeHandler} />
             </div>
